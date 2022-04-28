b64d802254090be5b04ce03149133de7edee7495
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-04-28 15:12:40 +0430

    added arrow function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 
-// Quiz 1
+//# Quiz 1
 
 function App() {
   return (
     <div>
       <Logo />
       <BestPokemon />
+      <Text />
     </div>
   );
 }
 
-// Quiz 2
+//# Quiz 2
 
 function Logo() {
   return (
@@ -30,5 +31,20 @@ function Logo() {
 function BestPokemon() {
   return <p>My favorite Pokemon is Squirtle</p>;
 }
+
+// # Arrow function
+const Text = () => {
+  return (
+    <div>
+      <h1>hello my friends</h1>
+    </div>
+  );
+};
+
+// const Text = () => (
+//   <div>
+//     <h1>hellooooo</h1>
+//   </div>
+// );
 
 export default App;
