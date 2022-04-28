b1ee0a6070a29d52b59e74235f338f9274bd374e
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-04-28 14:34:59 +0430

    quiz 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import './App.css';
+import "./App.css";
+
+// Quiz 1
 
 function App() {
   return (
     <div>
-      <header>
-        <div>
-          <h1>Welcome to the pokedex</h1>
-          <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="bird" />
-        </div>
-      </header>
+      <Logo />
+      <BestPokemon />
     </div>
   );
+}
+
+// Quiz 2
+
+function Logo() {
+  return (
+    <header>
+      <div>
+        <h1>Welcome to the pokedex</h1>
+        <img
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+          alt="bird"
+        />
+      </div>
+    </header>
+  );
+}
+
+function BestPokemon() {
+  return <p>My favorite Pokemon is Squirtle</p>;
 }
 
 export default App;
