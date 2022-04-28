942a6f178eea194ea934cc36d7209086b8eceb81
Taha <Taha.Hassanpour@outlook.com>
2022-04-28 14:15:50 +0430

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div>
+          <h1>Welcome to the pokedex</h1>
+          <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="bird" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
